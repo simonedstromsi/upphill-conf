25167c0abb379c92f87b2ccb872dc23f2ba29805
Robert Holma <robert.holma@screeninteraction.com>
2018-04-27 15:16:03 +0200

    Update title slide to Daresay fonts.

--- app/presentation/src/App.js
+++ app/presentation/src/App.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const TitleSlide = props => {
-  return <h1>{props.title}</h1>
+class TitleSlide extends Component{
+  render() {
+    return (
+      <div className='titleSlide'>
+        <h1 className='title'>{this.props.title}</h1>
+        <p className='logo'>DARESAY</p>
+      </div>
+    )
+  }
 }
 
 class App extends Component {
@@ -11,21 +18,21 @@ class App extends Component {
     currentSlide: 0,
     slides: [{
       type: "TitleSlide",
-      title: "Daresay at Uphill Conf"
+      title: "UPHILL CONF BERN 2018",
     },
     {
       type: "TitleSlide",
       title: "Another slide"
     }]
   }
-
+  
   componentDidMount(){
     document.addEventListener("keydown", this.onKeyDown, false);
   }
   componentWillUnmount(){
     document.removeEventListener("keydown", this.onKeyDown, false);
   }
-
+  
   onKeyDown = event => {
     if(event.keyCode == 39) {
       this.setState({
@@ -37,7 +44,7 @@ class App extends Component {
       })
     } 
   }
-
+  
   render() {
     const slide = this.state.slides[this.state.currentSlide]
     if(slide.type == "TitleSlide") {
