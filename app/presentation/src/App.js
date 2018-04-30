5d46211590e7b2b517cac2973ae7381bf9a634db
Erik Älgekrans <erik.algekrans@screeninteraction.com>
2018-04-30 22:25:55 +0200

    Rename components to styledComponents since that’s what it contains atm. Add a Slides factory class that will map elements/components to slide types.

--- app/presentation/src/App.js
+++ app/presentation/src/App.js
@@ -1,39 +1,8 @@
 import React, { Component } from 'react';
-import {Logo, WhiteLogo, Title, Slide, SlideCenter, SlideBottom, SlideCenterVertical, Island, Background, Video} from './Components';
-import { Player } from 'video-react';
+import { Logo, WhiteLogo, Title, Slide, SlideCenter, SlideBottom, SlideCenterVertical, Island, Background, Video } from './Components';
+import { Slides } from './Slides'
+
 import './App.css';
-
-
-class TitleSlide extends Component{
-  render() {
-    return (
-      <SlideCenterVertical>
-        <Title>{this.props.title}</Title>
-        <Logo/>
-      </SlideCenterVertical>
-    )
-  } 
-}
-
-const IslandSlide = props => {
-  return (
-    <SlideBottom>
-      <Background src={props.backgroundSrc}/>
-      <Island><h1>{props.title}</h1></Island>
-      <WhiteLogo/>
-    </SlideBottom>
-  )
-}
-
-const IslandVideoSlide = props => {
-  return (
-    <SlideBottom>
-      <Video src={props.videoSrc}/>
-      <Island><h1>{props.title}</h1></Island>
-      <WhiteLogo/>
-    </SlideBottom>
-  )
-}
 
 class App extends Component {
   state = {
@@ -64,14 +33,14 @@ class App extends Component {
   componentWillUnmount() {
     document.removeEventListener("keydown", this.onKeyDown, false);
   }
-  
+
   onKeyDown = event => {
     // Next
     if (event.keyCode === 39) {
       this.setState({
         currentSlide: Math.min(this.state.currentSlide + 1, this.state.slides.length - 1)
       })
-    } 
+    }
     // Prev
     else if (event.keyCode === 37) {
       this.setState({
@@ -80,23 +49,9 @@ class App extends Component {
     }
   }
 
-  getSlide(slide) {
-    if (slide.type === "TitleSlide") {
-      return <TitleSlide key={slide.type} { ...slide}/>
-    } else if (slide.type === "IslandSlide") {
-      return <IslandSlide key={slide.type} { ...slide}/>
-    } else if (slide.type === "IslandVideoSlide") {
-      return <IslandVideoSlide key={slide.type} { ...slide}/>
-    }
-    return null
-  }
-
-  renderSlides = () => {  
-    return this.getSlide(this.state.slides[this.state.currentSlide])  
-  }
-  
   render() {
-    return this.renderSlides()
+    const slide = this.state.slides[this.state.currentSlide]
+    return Slides.getSlide(slide.type, slide)
   }
 }
 
---
+++ app/presentation/src/Slides.js
@@ -0,0 +1,50 @@
+
+import React, { Component } from 'react';
+import { Logo, WhiteLogo, Title, Slide, SlideCenter, SlideBottom, SlideCenterVertical, Island, Background, Video } from './Components';
+import { Player } from 'video-react';
+
+class TitleSlide extends Component {
+    render() {
+        return (
+            <SlideCenterVertical>
+                <Title>{this.props.title}</Title>
+                <Logo />
+            </SlideCenterVertical>
+        )
+    }
+}
+
+const IslandSlide = props => {
+    return (
+        <SlideBottom>
+            <Background src={props.backgroundSrc} />
+            <Island><h1>{props.title}</h1></Island>
+            <WhiteLogo />
+        </SlideBottom>
+    )
+}
+
+const IslandVideoSlide = props => {
+    return (
+        <SlideBottom>
+            <Video src={props.videoSrc} />
+            <Island><h1>{props.title}</h1></Island>
+            <WhiteLogo />
+        </SlideBottom>
+    )
+}
+
+export const Slides = {
+    getSlide(type, props) {
+        switch (type) {
+            case "TitleSlide":
+                return <TitleSlide key={type} {...props} />
+            case "IslandSlide":
+                return <IslandSlide key={type} {...props} />
+            case "IslandVideoSlide":
+                return <IslandVideoSlide key={type} {...props} />
+            default:
+                return null
+        }
+    }
+}
